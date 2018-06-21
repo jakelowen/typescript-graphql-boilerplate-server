710780011ed3992950febb38cd72d566604cf500
Jake Lowen <jakelowen@gmail.com>
2018-06-21 12:10:48 -0500

    awad tutorial step 7

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,10 +1,10 @@
-import { Entity, Column, PrimaryColumn, BeforeInsert, BaseEntity } from "typeorm";
+import { Entity, Column, BeforeInsert, BaseEntity, PrimaryGeneratedColumn } from "typeorm";
 import * as uuidv4 from "uuid/v4"
 
 @Entity("users")
 export class User extends BaseEntity {
 
-    @PrimaryColumn("uuid")
+    @PrimaryGeneratedColumn("uuid")
     id: string;
 
     @Column("varchar", { length: 255 })
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -8,13 +8,25 @@ export const resolvers: ResolverMap = {
   },
   Mutation: {
     register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+      const userAlreadyExists = await User.findOne({ where: { email }, select: ["id"] })
+
+      if (userAlreadyExists) {
+        return [
+          {
+            path: "email",
+            message: "already taken"
+          }
+        ]
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10)
       const user = User.create({
         email,
         password: hashedPassword
       })
+
       await user.save()
-      return true
+      return null
     }
   }
 }
