13a9ae3810975ea159a3faaa40ce451e44678ef2
Jake Lowen <jakelowen@gmail.com>
2018-08-08 16:58:25 -0500

    adding tests for removePermissions mutation

--- src/modules/user/permissions/permissions.test.ts
+++ src/modules/user/permissions/permissions.test.ts
@@ -6,7 +6,13 @@ import * as faker from "faker";
 import { TestClientApollo } from "../../../utils/TestClientApollo";
 import gql from "graphql-tag";
 import * as bcrypt from "bcryptjs";
-import { noPermissionByName, noTeamExists, noUserExists, duplicatePermission, notTeamAdminError } from "./logic/errors";
+import {
+  noPermissionByName,
+  noTeamExists,
+  noUserExists,
+  duplicatePermission,
+  notTeamAdminError
+} from "./logic/errors";
 
 // import { redis } from "../../redis";
 
@@ -89,12 +95,14 @@ describe("addPermissionMutation", () => {
       email: faker.internet.email(),
       password: "foo"
     };
-    const permission = { id: faker.random.number(), name: faker.random.word().toUpperCase() };
+    const permission = {
+      id: faker.random.number(),
+      name: faker.random.word().toUpperCase()
+    };
 
     await db("teams").insert(team);
     await db("users").insert(user);
     await db("permissions").insert(permission);
-
 
     // console.log(team,user,permission)
     const testClient = new TestClientApollo(process.env.TEST_HOST as string);
@@ -102,24 +110,32 @@ describe("addPermissionMutation", () => {
       mutation: gql`
         mutation addPermission($input: addPermissionInput!) {
           addPermission(input: $input) {
-              error {
-                path
-                message
-              }
-              permission {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
                 id
-                name
-                team {
-                  id
-                }
               }
+            }
           }
         }
       `,
-      variables: { input: { permissionName: permission.name, userId: user.id, teamId: team.id } }
+      variables: {
+        input: {
+          permissionName: permission.name,
+          userId: user.id,
+          teamId: team.id
+        }
+      }
     });
-    expect((response.data as any).addPermission.error.length).toBe(1)
-    expect((response.data as any).addPermission.error[0].message).toEqual(notTeamAdminError)
+    expect((response.data as any).addPermission.error.length).toBe(1);
+    expect((response.data as any).addPermission.error[0].message).toEqual(
+      notTeamAdminError
+    );
   });
 
   test("works", async () => {
@@ -131,7 +147,7 @@ describe("addPermissionMutation", () => {
         id: faker.random.uuid(),
         email: faker.internet.email(),
         password: "foo"
-      }, 
+      },
       {
         email: faker.internet.email(),
         password: await bcrypt.hash(password, 10),
@@ -167,25 +183,35 @@ describe("addPermissionMutation", () => {
       mutation: gql`
         mutation addPermission($input: addPermissionInput!) {
           addPermission(input: $input) {
-              error {
-                path
-                message
-              }
-              permission {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
                 id
-                name
-                team {
-                  id
-                }
               }
+            }
           }
         }
       `,
-      variables: { input: { permissionName: permissions[0].name, userId: users[0].id, teamId: team.id } }
+      variables: {
+        input: {
+          permissionName: permissions[0].name,
+          userId: users[0].id,
+          teamId: team.id
+        }
+      }
     });
-    expect((response.data as any).addPermission.error).toBeNull()
-    expect((response.data as any).addPermission.permission.name).toEqual(permissions[0].name)
-    expect((response.data as any).addPermission.permission.team.id).toEqual(team.id)
+    expect((response.data as any).addPermission.error).toBeNull();
+    expect((response.data as any).addPermission.permission.name).toEqual(
+      permissions[0].name
+    );
+    expect((response.data as any).addPermission.permission.team.id).toEqual(
+      team.id
+    );
   });
 
   test("can not apply duplicate permission", async () => {
@@ -213,31 +239,41 @@ describe("addPermissionMutation", () => {
       mutation: gql`
         mutation addPermission($input: addPermissionInput!) {
           addPermission(input: $input) {
-              error {
-                path
-                message
-              }
-              permission {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
                 id
-                name
-                team {
-                  id
-                }
               }
+            }
           }
         }
       `,
-      variables: { input: { permissionName: permission.name, userId: user.id, teamId: team.id } }
+      variables: {
+        input: {
+          permissionName: permission.name,
+          userId: user.id,
+          teamId: team.id
+        }
+      }
     });
-    expect((response.data as any).addPermission.error.length).toBe(1)
-    expect((response.data as any).addPermission.error[0].message).toEqual(duplicatePermission)
-
+    expect((response.data as any).addPermission.error.length).toBe(1);
+    expect((response.data as any).addPermission.error[0].message).toEqual(
+      duplicatePermission
+    );
   });
 
   test("user does not exist error", async () => {
     const team = { id: faker.random.uuid(), name: faker.company.companyName() };
     await db("teams").insert(team);
-    const permission = { id: faker.random.number(), name: faker.random.word().toUpperCase() };
+    const permission = {
+      id: faker.random.number(),
+      name: faker.random.word().toUpperCase()
+    };
     await db("permissions").insert(permission);
 
     const testClient = new TestClientApollo(process.env.TEST_HOST as string);
@@ -245,28 +281,39 @@ describe("addPermissionMutation", () => {
       mutation: gql`
         mutation addPermission($input: addPermissionInput!) {
           addPermission(input: $input) {
-              error {
-                path
-                message
-              }
-              permission {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
                 id
-                name
-                team {
-                  id
-                }
               }
+            }
           }
         }
       `,
-      variables: { input: { permissionName: permission.name, userId: faker.random.uuid(), teamId: team.id } }
+      variables: {
+        input: {
+          permissionName: permission.name,
+          userId: faker.random.uuid(),
+          teamId: team.id
+        }
+      }
     });
-    expect((response.data as any).addPermission.error.length).toBe(1)
-    expect((response.data as any).addPermission.error[0].message).toEqual(noUserExists)
+    expect((response.data as any).addPermission.error.length).toBe(1);
+    expect((response.data as any).addPermission.error[0].message).toEqual(
+      noUserExists
+    );
   });
 
   test("team does not exist error", async () => {
-    const permission = { id: faker.random.number(), name: faker.random.word().toUpperCase() };
+    const permission = {
+      id: faker.random.number(),
+      name: faker.random.word().toUpperCase()
+    };
     await db("permissions").insert(permission);
 
     const testClient = new TestClientApollo(process.env.TEST_HOST as string);
@@ -274,24 +321,32 @@ describe("addPermissionMutation", () => {
       mutation: gql`
         mutation addPermission($input: addPermissionInput!) {
           addPermission(input: $input) {
-              error {
-                path
-                message
-              }
-              permission {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
                 id
-                name
-                team {
-                  id
-                }
               }
+            }
           }
         }
       `,
-      variables: { input: { permissionName: permission.name, userId: faker.random.uuid(), teamId: faker.random.uuid() } }
+      variables: {
+        input: {
+          permissionName: permission.name,
+          userId: faker.random.uuid(),
+          teamId: faker.random.uuid()
+        }
+      }
     });
-    expect((response.data as any).addPermission.error.length).toBe(1)
-    expect((response.data as any).addPermission.error[0].message).toEqual(noTeamExists)
+    expect((response.data as any).addPermission.error.length).toBe(1);
+    expect((response.data as any).addPermission.error[0].message).toEqual(
+      noTeamExists
+    );
   });
 
   test("permission does not exist error", async () => {
@@ -300,22 +355,284 @@ describe("addPermissionMutation", () => {
       mutation: gql`
         mutation addPermission($input: addPermissionInput!) {
           addPermission(input: $input) {
-              error {
-                path
-                message
-              }
-              permission {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
                 id
-                name
-                team {
-                  id
-                }
               }
+            }
           }
         }
       `,
-      variables: { input: { permissionName: faker.random.word(), userId: faker.random.uuid(), teamId: faker.random.uuid() } }
+      variables: {
+        input: {
+          permissionName: faker.random.word(),
+          userId: faker.random.uuid(),
+          teamId: faker.random.uuid()
+        }
+      }
     });
-    expect((response.data as any).addPermission.error.length).toBe(1)
-    expect((response.data as any).addPermission.error[0].message).toEqual(noPermissionByName)
+    expect((response.data as any).addPermission.error.length).toBe(1);
+    expect((response.data as any).addPermission.error[0].message).toEqual(
+      noPermissionByName
+    );
+  });
+});
+
+describe("removePermissionMutation", () => {
+  test("can not remove if not team admin", async () => {
+    const team = { id: faker.random.uuid(), name: faker.company.companyName() };
+    const user = {
+      id: faker.random.uuid(),
+      email: faker.internet.email(),
+      password: "foo"
+    };
+    const permission = {
+      id: faker.random.number(),
+      name: faker.random.word().toUpperCase()
+    };
+
+    await db("teams").insert(team);
+    await db("users").insert(user);
+    await db("permissions").insert(permission);
+
+    // console.log(team,user,permission)
+    const testClient = new TestClientApollo(process.env.TEST_HOST as string);
+    const response = await testClient.client.mutate({
+      mutation: gql`
+        mutation removePermission($input: removePermissionInput!) {
+          removePermission(input: $input) {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
+                id
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        input: {
+          permissionName: permission.name,
+          userId: user.id,
+          teamId: team.id
+        }
+      }
+    });
+    expect((response.data as any).removePermission.error.length).toBe(1);
+    expect((response.data as any).removePermission.error[0].message).toEqual(
+      notTeamAdminError
+    );
+  });
+
+  test("works", async () => {
+    const team = { id: faker.random.uuid(), name: faker.company.companyName() };
+    const password = faker.internet.password();
+
+    const users = [
+      {
+        id: faker.random.uuid(),
+        email: faker.internet.email(),
+        password: "foo"
+      },
+      {
+        email: faker.internet.email(),
+        password: await bcrypt.hash(password, 10),
+        id: faker.random.uuid(),
+        confirmed: true
+      }
+    ];
+
+    const permissions = [
+      {
+        id: faker.random.number(),
+        name: faker.random.word().toUpperCase()
+      },
+      { id: faker.random.number(), name: "ADMIN" }
+    ];
+
+    const gtps = [
+      {
+        id: faker.random.uuid(),
+        permissionId: permissions[1].id,
+        userId: users[1].id,
+        teamId: team.id
+      },
+      {
+        id: faker.random.uuid(),
+        permissionId: permissions[0].id,
+        userId: users[0].id,
+        teamId: team.id
+      }
+    ];
+
+    await db("teams").insert(team);
+    await db("users").insert(users);
+    await db("permissions").insert(permissions);
+    await db("granted_team_permissions").insert(gtps);
+
+    // console.log(team,user,permission)
+    const testClient = new TestClientApollo(process.env.TEST_HOST as string);
+
+    // login
+    await testClient.login(users[1].email, password);
+
+    const response = await testClient.client.mutate({
+      mutation: gql`
+        mutation removePermission($input: removePermissionInput!) {
+          removePermission(input: $input) {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
+                id
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        input: {
+          permissionName: permissions[0].name,
+          userId: users[0].id,
+          teamId: team.id
+        }
+      }
+    });
+    expect((response.data as any).removePermission.error).toBeNull();
+    const dbGTP = await db("granted_team_permissions")
+      .where({ id: gtps[1].id })
+      .first();
+    expect(dbGTP).toBeUndefined();
+  });
+
+  test("user does not exist error", async () => {
+    const team = { id: faker.random.uuid(), name: faker.company.companyName() };
+    await db("teams").insert(team);
+    const permission = {
+      id: faker.random.number(),
+      name: faker.random.word().toUpperCase()
+    };
+    await db("permissions").insert(permission);
+
+    const testClient = new TestClientApollo(process.env.TEST_HOST as string);
+    const response = await testClient.client.mutate({
+      mutation: gql`
+        mutation removePermission($input: removePermissionInput!) {
+          removePermission(input: $input) {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
+                id
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        input: {
+          permissionName: permission.name,
+          userId: faker.random.uuid(),
+          teamId: team.id
+        }
+      }
+    });
+    expect((response.data as any).removePermission.error.length).toBe(1);
+    expect((response.data as any).removePermission.error[0].message).toEqual(
+      noUserExists
+    );
+  });
+
+  test("team does not exist error", async () => {
+    const permission = {
+      id: faker.random.number(),
+      name: faker.random.word().toUpperCase()
+    };
+    await db("permissions").insert(permission);
+
+    const testClient = new TestClientApollo(process.env.TEST_HOST as string);
+    const response = await testClient.client.mutate({
+      mutation: gql`
+        mutation removePermission($input: removePermissionInput!) {
+          removePermission(input: $input) {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
+                id
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        input: {
+          permissionName: permission.name,
+          userId: faker.random.uuid(),
+          teamId: faker.random.uuid()
+        }
+      }
+    });
+    expect((response.data as any).removePermission.error.length).toBe(1);
+    expect((response.data as any).removePermission.error[0].message).toEqual(
+      noTeamExists
+    );
+  });
+
+  test("permission does not exist error", async () => {
+    const testClient = new TestClientApollo(process.env.TEST_HOST as string);
+    const response = await testClient.client.mutate({
+      mutation: gql`
+        mutation removePermission($input: removePermissionInput!) {
+          removePermission(input: $input) {
+            error {
+              path
+              message
+            }
+            permission {
+              id
+              name
+              team {
+                id
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        input: {
+          permissionName: faker.random.word(),
+          userId: faker.random.uuid(),
+          teamId: faker.random.uuid()
+        }
+      }
+    });
+    expect((response.data as any).removePermission.error.length).toBe(1);
+    expect((response.data as any).removePermission.error[0].message).toEqual(
+      noPermissionByName
+    );
+  });
 });
--- todo.md
+++ todo.md
@@ -1,12 +1,5 @@
 ## NEXT UP
 
-- addTeamPermision Mutation... Only team admin can add a team permission.
-
-  - potential errors: permission does not exist.
-  - user does not exist.
-  - You don't have admin rights.
-
-- add logging library middleware
 - Deploy to now.sh
 - turn back on user emailing stuff, consider abstracting mailer out into object in context and include mocked instance in testing?
 
